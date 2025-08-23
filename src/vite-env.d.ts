531b58941f896b6cd54acfff7c1e2be519fa1da3
Aditya Patel <heyadityapatel@gmail.com>
2025-08-23 21:07:24 +0530

    trying to get this to work

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,9 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_GMAIL_CLIENT_ID: string;
+  readonly VITE_GMAIL_REDIRECT_URI: string;
+  readonly VITE_GMAIL_TOKEN_ENDPOINT: string;
 }
 
 interface ImportMeta {
