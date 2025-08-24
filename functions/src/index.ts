5acb48992c873df0db3c312d5d28b1c06e4a19f2
Aditya Patel <aditya.patel@hotwax.co>
2025-08-24 13:26:20 +0530

    chore(functions): upgrade firebase-functions and firebase-admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions/v1';
 import admin from 'firebase-admin';
 import { getGmailClient, getOAuth2Client } from './gmailAuth.js';
 import { GoogleGenerativeAI } from '@google/generative-ai';
